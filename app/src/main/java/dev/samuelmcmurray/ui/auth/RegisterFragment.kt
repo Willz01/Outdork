e06c059a42407cb189706c2cd7bad42dbe96cd11
Wills Ekanem <ekanemwills1@gmail.com>
2021-02-23 12:25:29 +0100

    Revert "Merge branch 'main' into FacebookShare"
    This reverts commit a2355a62aacc79638919b8f27c332c48c0021b74, reversing
    changes made to 69cc46662702dcd9b50c1d7f5942695981fe0f11.

--- app/src/main/java/dev/samuelmcmurray/ui/auth/RegisterFragment.kt
+++ app/src/main/java/dev/samuelmcmurray/ui/auth/RegisterFragment.kt
@@ -51,8 +51,6 @@ class RegisterFragment : Fragment() {
 
         val signUpButton = binding.buttonSignUp
         val cancelButton = binding.buttonCancel
-        val passwordText = binding.editTextPassword
-        val passwordConfirmText = binding.editTextPassword2
 
         signUpButton.setOnClickListener {
             val firstNameText = binding.editTextFirstName
@@ -107,7 +105,7 @@ class RegisterFragment : Fragment() {
                         requireContext().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
                     imm.hideSoftInputFromWindow(requireView().windowToken, 0)
                 }
-                Log.d(TAG, "login: " + fireBaseUser.uid)
+                Log.d(TAG, "login: " + fireBaseUser.toString())
             })
         } else {
             if (password != passwordConfirm) {

3e19e15ce64e0b7db7d56f8a0abccab8b0cd6001
sam-mcmurray <smcmurray1126@gmail.com>
2021-02-25 13:29:32 +0100

    Changed following pager adapter to non depricated inheritance

--- app/src/main/java/dev/samuelmcmurray/ui/following/FollowingHolder.kt
+++ app/src/main/java/dev/samuelmcmurray/ui/following/FollowingHolder.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.inflate
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import dev.samuelmcmurray.R
 import dev.samuelmcmurray.databinding.FragmentFollowingHolderBinding
 import dev.samuelmcmurray.ui.following.feeds.FollowingFeedsFragment
@@ -24,7 +25,7 @@ class FollowingHolder : Fragment() {
     private lateinit var binding : FragmentFollowingHolderBinding
 
     private lateinit var tabLayout: TabLayout
-    private lateinit var viewPager : ViewPager
+    private lateinit var viewPager : ViewPager2
 
     private lateinit var followingPagerAdapter: FollowingPagerAdapter
 
@@ -36,19 +37,21 @@ class FollowingHolder : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_following_holder,container,false)
+        binding = inflate(inflater,R.layout.fragment_following_holder,container,false)
         binding.lifecycleOwner = this
 
         tabLayout = binding.tabLayout
         viewPager = binding.viewPager
 
-        followingPagerAdapter = FollowingPagerAdapter(this.childFragmentManager)
+        followingPagerAdapter = FollowingPagerAdapter(requireActivity())
 
         followingPagerAdapter.addFragment(FollowingFeedsFragment(),"Feeds")
         followingPagerAdapter.addFragment(FollowingListFragment(),"Following")
 
         viewPager.adapter = followingPagerAdapter
-        tabLayout.setupWithViewPager(viewPager)
+        TabLayoutMediator(tabLayout, viewPager)  { currentTab, currentPosition ->
+            currentTab.text = followingPagerAdapter.getPageTitle(currentPosition)
+        }.attach()
 
 
         return binding.root

409931a57e6d6ff8b62f7f6b7d046a78491cbb48
Lukas <57799079+OlssonLukas@users.noreply.github.com>
2021-03-06 18:08:37 +0100

    Update ViewPagerAdapter.kt

--- app/src/main/java/dev/samuelmcmurray/ui/profile/ViewPagerAdapter.kt
+++ app/src/main/java/dev/samuelmcmurray/ui/profile/ViewPagerAdapter.kt
@@ -7,13 +7,6 @@ import androidx.viewpager2.widget.ViewPager2
 
 class ViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
-    private lateinit var fragmentList: ArrayList<Fragment>
-    private lateinit var stringList: ArrayList<String>
-
-    private lateinit var viewPager: ViewPager2
-
-
-
     override fun getItemCount(): Int = 100
     private val ARG_OBJECT = "object"
 
@@ -21,7 +14,7 @@ class ViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
     override fun createFragment(position: Int): Fragment {
         val fragment = OtherProfileFragment()
         fragment.arguments = Bundle().apply {
-            // Our object is just an integer :-P
+
             putInt(ARG_OBJECT, position + 1)
         }
         return fragment

1bf26936df6b0a39e298fef83795a09504851746
Lukas <57799079+OlssonLukas@users.noreply.github.com>
2021-03-08 17:35:29 +0100

    work

--- app/src/main/java/dev/samuelmcmurray/ui/profile/OtherProfileFragment.kt
+++ app/src/main/java/dev/samuelmcmurray/ui/profile/OtherProfileFragment.kt
@@ -6,12 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import dev.samuelmcmurray.R
+import dev.samuelmcmurray.databinding.FragmentOtherProfileBinding
+import dev.samuelmcmurray.ui.following.feeds.FollowingFeedsFragment
 
 class OtherProfileFragment : Fragment() {
 
+    companion object {
+        fun newInstance() = OtherProfileFragment()
+    }
+
+    private lateinit var binding : FragmentOtherProfileBinding
+
     private lateinit var viewPagerAdapter: ViewPagerAdapter
     private lateinit var viewPager: ViewPager2
+
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,8 +34,15 @@ class OtherProfileFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        viewPagerAdapter = ViewPagerAdapter(this)
         viewPager = view.findViewById(R.id.view_pager)
-        viewPager.adapter = viewPagerAdapter
+        tabLayout = view.findViewById(R.id.tab_layout)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = "Tab " + position + 1
+            viewPager.setCurrentItem(tab.position, true)
+        }.attach()
+//        viewPagerAdapter = ViewPagerAdapter(this)
+//        viewPager = view.findViewById(R.id.view_pager)
+//        viewPager.adapter = viewPagerAdapter
     }
 }
